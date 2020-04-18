09e12d08673cba4173ba8b1794e57adbc0c6e82c
Cyriaque <cyriaquedelaunay@gmail.com>
2020-04-18 11:56:42 +0200

    added test assets

---
+++ src/assets/graphics/landscape/road.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.3.4" name="road" tilewidth="64" tileheight="64" tilecount="24" columns="8">
+ <image source="road.png" width="512" height="192"/>
+</tileset>
--- src/scripts/scenes/MainScene.ts
+++ src/scripts/scenes/MainScene.ts
@@ -1,6 +1,7 @@
 import "phaser";
 
 export default class MainScene extends Phaser.Scene {
+  tilemap: Phaser.Tilemaps.Tilemap;
   constructor() {
     super({
       key: "MainScene",
@@ -8,9 +9,14 @@ export default class MainScene extends Phaser.Scene {
   }
   preload() {
     this.load.image("gameImage", "assets/graphics/ui/game.png");
+    this.load.tilemapTiledJSON('map', 'assets/levels/level1.json');
+    this.load.image('road', 'assets/graphics/road.png');
   }
   create() {
-    this.add.image(400, 300, "gameImage");
+    // this.add.image(400, 300, "gameImage");
+    this.tilemap = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
+    const tileset = this.tilemap.addTilesetImage('gameImage', null, 32, 32, 1, 2);
+    const layer = this.tilemap.createStaticLayer(0, tileset, 0, 0);
   }
 
 }
