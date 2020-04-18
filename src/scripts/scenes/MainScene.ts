5adc67ca18452298f2007a6542d3dd130909865a
Cyriaque <cyriaquedelaunay@gmail.com>
2020-04-18 12:56:11 +0200

    //WIP

--- src/scripts/scenes/MainScene.ts
+++ src/scripts/scenes/MainScene.ts
@@ -9,12 +9,13 @@ export default class MainScene extends Phaser.Scene {
   }
   preload() {
     this.load.image("gameImage", "assets/graphics/ui/game.png");
-    this.load.tilemapTiledJSON('map', 'assets/levels/level1.json');
     this.load.image('road', 'assets/graphics/road.png');
+    this.load.spritesheet('road', 'assets/graphics/road.tsx');
+    this.load.tilemapTiledJSON('map', 'assets/levels/level1.json');
   }
   create() {
     // this.add.image(400, 300, "gameImage");
-    this.tilemap = this.make.tilemap({ key: 'map', tileWidth: 32, tileHeight: 32 });
+    this.tilemap = this.make.tilemap({ key: 'map', tileWidth: 30, tileHeight: 20 });
     const tileset = this.tilemap.addTilesetImage('gameImage', null, 32, 32, 1, 2);
     const layer = this.tilemap.createStaticLayer(0, tileset, 0, 0);
   }
